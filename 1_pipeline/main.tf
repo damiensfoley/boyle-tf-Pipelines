121f23f98e0563cd35c558d7bddea1d7ead06782
damiensfoley <damiensfoley@gmail.com>
2025-08-29 15:43:29 +0100

    Updated to resolve errors on TF init

--- 1_pipeline/main.tf
+++ 1_pipeline/main.tf
@@ -1,16 +1,17 @@
 terraform {
   backend "s3" {
     bucket         = "df-terraform-remote-state-s3"
-    key            = "df-terraform-remote-state-s3.tfstate"
-    region         = "eu-central-1"
+    key            = "df-terraformf-remote-state-s3.tfstate"
+    region         = "eu-west-1"
     encrypt        = true
-    dynamodb_table = "df-terraform-remote-state-dynamodb"
+    use_lockfile = true
+    dynamodb_table = "df-tf-locks-table"
   }
 }
 
 locals {
-    aws_region  = "eu-central-1"
-    prefix      = "df-terraform-remote-state"
+    aws_region  = "eu-west-1"
+    prefix      = "df-tf-remote-state"
     ssm_prefix  = "/df/terraform"
     common_tags = {
         Project         = "df-Terraform-AWS"
--- 1_pipeline/ssm_parameters.tf
+++ 1_pipeline/ssm_parameters.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 data "aws_ssm_parameter" "remote_state_bucket" {
-  name = "${local.ssm_prefix}/df-tf-remote-state-bucket"
+  name = "${local.ssm_prefix}/df-terraform-remote-state-bucket"
 }
 
 data "aws_ssm_parameter" "locks_table_arn" {
